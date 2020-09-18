723b7d903dd3d056f6654fe617402f5c64c634a5
qdork <seanroughton@gmail.com>
2020-09-18 12:58:31 +0100

    Commit to master - added gitignore

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var el = document.createElement('div');
 el.setAttribute('class', 'only-el');
 el.innerHTML = 'Hello universe!';
+
+var el2 = document.createElement('div');
+
+el2.innerHTML = 'Also, hello to the world';
